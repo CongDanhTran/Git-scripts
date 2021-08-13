59e8bc3b558c7e3f6c1f2e78cff89d61d5046010
CongDanhTran <agetuthanno1@yahoo.com.vn>
2021-08-13 10:35:37 +0100

    Add files via upload

---
+++ autocomplete-1-run-only.bat
@@ -0,0 +1,2 @@
+echo. >> "%userprofile%/.bash_profile"
+echo source ~/gc-completion.bash >> "%userprofile%/.bash_profile"
---
+++ install.bat
@@ -0,0 +1,4 @@
+xcopy.exe "%~dp0\script\gc" "C:\Program Files\Git\mingw64\bin" /y
+xcopy.exe "%~dp0\script\gg" "C:\Program Files\Git\mingw64\bin" /y
+xcopy.exe "%~dp0\script\gc-completion.bash" "%userprofile%" /y
+pause
